b673a7117f67cd80e419c716681cadfc71781097
Dev-organico <joao.zolet@gmail.com>
2023-02-09 23:22:24 -0300

    CRUD de Jogos [Create | Read]

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -1,18 +1,40 @@
-import {db} from "../config/database.js"
+import { db } from "../config/database.js"
 
 
 
-export async function listGames(req,res){
+export async function listGames(req, res) {
     try {
-        
+        const games = await db.query(`SELECT * FROM games`)
 
-
+        res.send(games.rows)
 
     } catch (error) {
         res.status(500).send(error.message)
     }
 }
 
-export async function insertGames(req,res){
-    return('oi')
+export async function insertGames(req, res) {
+
+    const {
+        name,
+        image,
+        stockTotal,
+        pricePerDay } = req.body
+
+    try {
+
+        const exist = await db.query(`SELECT * FROM games WHERE name = $1`,[name])
+
+        if(exist.rows.length > 0) return res.sendStatus(409)
+
+        const singleGame = await db.query(`INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1,$2,$3,$4);`,[name,image,stockTotal,pricePerDay])
+
+        console.log(singleGame)
+
+        res.sendStatus(201)
+
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
--- src/schema/gameSchema.js
+++ src/schema/gameSchema.js
@@ -1,7 +1,13 @@
 import joi from "joi";
 
 export const gameSchema = joi.object({
-  value: joi.number().required(),
-  description: joi.string().required(),
-  type: joi.string().valid("positive", "negative").required()
+  name: joi.string().required().min(1),
+  image: joi.string().required(),
+  stockTotal: joi.number().positive(),
+  pricePerDay: joi.number().positive()
+
 });
+
+ /*  name: joi.string().required().min(1),
+  description: joi.string().required(),
+  type: joi.string().valid("positive", "negative").required() */
