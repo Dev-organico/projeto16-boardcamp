c4071993a77cad4f98ebe422c6a0670234a0edce
Dev-organico <joao.zolet@gmail.com>
2023-02-09 23:48:13 -0300

    CRUD de Jogos [Create | Read]

--- src/schema/gameSchema.js
+++ src/schema/gameSchema.js
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 export const gameSchema = joi.object({
-  name: joi.string().required(),
+  name: joi.string().required().min(1),
   image: joi.string().required(),
   stockTotal: joi.number().positive(),
   pricePerDay: joi.number().positive()
